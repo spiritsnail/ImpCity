103f1f87600c795243be96791dac89f8ce13f9af
Adam Shamma <mayodeftinwolf@yahoo.com>
2015-11-27 14:10:22 -0500

    Add Imp City 1.2.py

--- Imp City 1.2.py
+++ Imp City 1.2.py
@@ -8,19 +8,19 @@ import math
 ##Input Parameters
 
 #initial distribution of Imps
-plants = []
+impLocations = []
 
 #GENERATE THE INITIAL Locations OF PLANTS
 for cc0 in range(0,12):
     for cc1 in range(0,13):
-        plants.append(numpy.array([cc0, cc1]))
+        impLocations.append(numpy.array([cc0, cc1]))
         
-noi = len(plants)
-foodlist = numpy.ones([1,len(plants)])[0]
+noi = len(impLocations)
+foodlist = numpy.ones([1,len(impLocations)])[0]
 
 #egalitarianism
 #the maximum more than your share that a relationship can be
-egalit = 100
+egalitarianism = 100
 
 #base traits
 baseT = numpy.array([10, 10, 10])
@@ -29,24 +29,23 @@ baseT = numpy.array([10, 10, 10])
 variation = 10/3  #10 being average
 entropy = 1/3     #10 being average
 
-coefProximityVariation = 1/3
-cPVEntropy = 1/30
+proximityModifierVariation = 1/3
+proximityModifierEntropy = 1/30
 
-coefMemoryVariation = 1/3
-cMVEntropy = 1/30
+memoryModifierVariation = 1/3
+memoryModifierEntropy = 1/30
 
 #reproduction constants
 floweringDistance = 1/3
 floweringStddev = 1/12
 
-#prisdel
-#basic prisoner's dilemma
-pdel = numpy.array([[-.25,0,.3],[-.05,0,.05],[-.4,0,.2]])
+#expanded prisoner's dilemma
+consequenceMatrix = numpy.array([[-.25,0,.3],[-.05,0,.05],[-.4,0,.2]])
 
 #CRUNCH
 bases = [] #base behavior matrix (1,3)
-pms   = [] #proximity coefficient vectors (1,3)
-mms   = [] #memory coefficient matricies  (3,3)
+proximityModifers   = [] #proximity coefficient vectors (1,3)
+memoryModifers   = [] #memory coefficient matricies  (3,3)
 isdead = []
 gens = 1
 megakill = 0
@@ -87,12 +86,12 @@ def makeBases():
 def reproduce(parent):
     loc = numpy.zeros([1,2])[0]
     direc = random.random()*2*math.pi
-    loc[0] = plants[parent][0] + random.normalvariate(floweringDistance,floweringStddev) * math.cos(direc)
-    loc[1] = plants[parent][1] + random.normalvariate(floweringDistance,floweringStddev) * math.sin(direc)
-    mmMod = numpy.random.normal(0,cMVEntropy,[3,3])
-    mmNew = mms[parent] + mmMod
-    pmMod = numpy.random.normal(0,cPVEntropy,[1,3])
-    pmNew = pms[parent] + pmMod
+    loc[0] = impLocations[parent][0] + random.normalvariate(floweringDistance,floweringStddev) * math.cos(direc)
+    loc[1] = impLocations[parent][1] + random.normalvariate(floweringDistance,floweringStddev) * math.sin(direc)
+    mmMod = numpy.random.normal(0,memoryModifierEntropy,[3,3])
+    mmNew = memoryModifers[parent] + mmMod
+    pmMod = numpy.random.normal(0,proximityModifierEntropy,[1,3])
+    pmNew = proximityModifers[parent] + pmMod
     baseMod = numpy.random.normal(0,entropy,[1,3])[0]
     newBases = bases[parent] + baseMod
     return [loc, mmNew, pmNew, newBases]
@@ -100,26 +99,24 @@ def reproduce(parent):
 #populate the characteristics of the initial set of Imps
 for cc in range(0, noi):
     bases.append(makeBases())
-    pms.append(numpy.random.normal(0,coefProximityVariation,[1,3]))
-    mms.append(numpy.random.normal(0,coefProximityVariation,[3,3]))
+    proximityModifers.append(numpy.random.normal(0,proximityModifierVariation,[1,3]))
+    memoryModifers.append(numpy.random.normal(0,proximityModifierVariation,[3,3]))
     
 ml = makeMemoryList(noi)
 
 #Generate a proximity list
 #proximity is the inverse square of distance
-proxlist = numpy.zeros([3, .5*len(plants)*(len(plants)-1)])
-#proxmat = numpy.zeros([len(plants),len(plants)])
+proxlist = numpy.zeros([3, .5*len(impLocations)*(len(impLocations)-1)])
 ccd1 = 0
 tct = 0
-while ccd1 < len(plants):
-    for ccd2 in range(ccd1+1, len(plants)):
+while ccd1 < len(impLocations):
+    for ccd2 in range(ccd1+1, len(impLocations)):
         proxlist[0][tct] = ccd1
         proxlist[1][tct] = ccd2
-        dis = numpy.linalg.norm(plants[ccd1]-plants[ccd2])
-        proxlist[2][tct] = 1/(dis*dis)
+        dis = numpy.linalg.norm(impLocations[ccd1]-impLocations[ccd2])
+        proxlist[2][tct] = 1/(dis*dis) #proximity
         tct += 1
-        #proxmat[ccd1,ccd2] = 1/(dis*dis)
-        #proxmat[ccd2,ccd1] = 1/(dis*dis)
+
     ccd1 += 1
 
 while megakill == 0:
@@ -132,42 +129,42 @@ while megakill == 0:
         proximity = proxlist[2][chosepair]   
         
         #generate the final decision vector of plant 1
-        fdec1 = bases[imp1]
-        fdec1 = fdec1 + (pms[imp1][0] * proximity)
-        fdec1 = fdec1 + sum(mms[imp1] * numpy.array([[ml[imp1][0][imp2]],
+        finalDecisionVector1 = bases[imp1]
+        finalDecisionVector1 = finalDecisionVector1 + (proximityModifers[imp1][0] * proximity)
+        finalDecisionVector1 = finalDecisionVector1 + sum(memoryModifers[imp1] * numpy.array([[ml[imp1][0][imp2]],
                                                     [ml[imp1][1][imp2]],
                                                     [ml[imp1][2][imp2]]]))
         #choose an action for plant 1
-        if max(fdec1) <= 0:
-            fdec1 = numpy.array([1,1,1])
+        if max(finalDecisionVector1) <= 0:
+            finalDecisionVector1 = numpy.array([1,1,1])
         
-        if min(fdec1) < 0:
+        if min(finalDecisionVector1) < 0:
             for cc1 in range(0,3):
-                if fdec1[cc1] < 0:
-                    fdec1[cc1] = 0
+                if finalDecisionVector1[cc1] < 0:
+                    finalDecisionVector1[cc1] = 0
                     
-        choice1 = wre(fdec1)
+        choice1 = wre(finalDecisionVector1)
         
         #generate the final decision vector of plant 2
-        fdec2 = bases[imp2]
-        fdec2 = fdec2 + (pms[imp2][0] * proximity)
-        fdec2 = fdec2 + sum(mms[imp2] * numpy.array([[ml[imp2][0][imp1]],
+        finalDecisionVector2 = bases[imp2]
+        finalDecisionVector2 = finalDecisionVector2 + (proximityModifers[imp2][0] * proximity)
+        finalDecisionVector2 = finalDecisionVector2 + sum(memoryModifers[imp2] * numpy.array([[ml[imp2][0][imp1]],
                                                     [ml[imp2][1][imp1]],
                                                     [ml[imp2][2][imp1]]]))
         #choose an action for plant 2
-        if max(fdec2) <= 0:
-            fdec2 = numpy.array([1,1,1])
+        if max(finalDecisionVector2) <= 0:
+            finalDecisionVector2 = numpy.array([1,1,1])
         
-        if min(fdec2) < 0:
+        if min(finalDecisionVector2) < 0:
             for cc2 in range(0,3):
-                if fdec2[cc2] < 0:
-                    fdec2[cc2] = 0
+                if finalDecisionVector2[cc2] < 0:
+                    finalDecisionVector2[cc2] = 0
         
-        choice2 = wre(fdec2)
+        choice2 = wre(finalDecisionVector2)
         
         #modify the food and memories of the paticipants
-        foodlist[imp1] += pdel[choice1][choice2]
-        foodlist[imp2] += pdel[choice2][choice1]
+        foodlist[imp1] += consequenceMatrix[choice1][choice2]
+        foodlist[imp2] += consequenceMatrix[choice2][choice1]
         
         ml[imp1][choice2][imp2] += 1
         ml[imp2][choice1][imp1] += 1
@@ -187,7 +184,6 @@ while megakill == 0:
             #generate a new proximity list without the missing plants
             if newprox:
                 proxlist = numpy.zeros([3, .5*(noi - len(isdead))*(noi - len(isdead) - 1)])
-                #proxmat = numpy.zeros([len(plants),len(plants)])
                 ccd1 = 0
                 tct = 0
                 while ccd1 < noi:
@@ -195,40 +191,40 @@ while megakill == 0:
                         if ccd1 not in isdead and ccd2 not in isdead:
                             proxlist[0][tct] = ccd1
                             proxlist[1][tct] = ccd2
-                            dis = numpy.linalg.norm(plants[ccd1]-plants[ccd2])
+                            dis = numpy.linalg.norm(impLocations[ccd1]-impLocations[ccd2])
                             proxlist[2][tct] = 1/(dis*dis)
                             tct += 1
                     ccd1 += 1
                 
-                proxlist[2]= numpy.clip(proxlist[2::][0], 0, egalit*numpy.average(proxlist[2::][0]))
+                proxlist[2]= numpy.clip(proxlist[2::][0], 0, egalitarianism*numpy.average(proxlist[2::][0]))
     #We are done with a generation. Now reproduce.
     
     #return [loc, mmNew, pmNew, newBases]
     
     ngloc = []
-    ngmms = []
-    ngpms = []
+    ngmemoryModifers = []
+    ngproximityModifers = []
     ngbases = []
     
     for ccf in range(0,len(foodlist)):
         if ccf not in isdead:
             for ccb in range(0, math.floor(foodlist[ccf])):
                 ngloc.append(reproduce(ccf)[0])
-                ngmms.append(reproduce(ccf)[1])
-                ngpms.append(reproduce(ccf)[2])
+                ngmemoryModifers.append(reproduce(ccf)[1])
+                ngproximityModifers.append(reproduce(ccf)[2])
                 ngbases.append(reproduce(ccf)[3])
             #print(ccf)
             runt = random.random()
             if runt < foodlist[ccf]%1:
                 ngloc.append(reproduce(ccf)[0])
-                ngmms.append(reproduce(ccf)[1])
-                ngpms.append(reproduce(ccf)[2])
+                ngmemoryModifers.append(reproduce(ccf)[1])
+                ngproximityModifers.append(reproduce(ccf)[2])
                 ngbases.append(reproduce(ccf)[3])
             #print(ccf)
     
     print(numpy.average(bases,0))
-    print(numpy.average(pms,0))
-    print(len(plants))
+    print(numpy.average(proximityModifers,0))
+    print(len(impLocations))
     print("*****")
     
     if len(ngloc)>500:
@@ -239,29 +235,29 @@ while megakill == 0:
         megakill = 1
     else:
         gens += 1
-        plants = ngloc
-        mms = ngmms
-        pms = ngpms
+        impLocations = ngloc
+        memoryModifers = ngmemoryModifers
+        proximityModifers = ngproximityModifers
         bases = ngbases
         isdead = []
         
-        noi = len(plants)
-        foodlist = numpy.ones([1,len(plants)])[0]
+        noi = len(impLocations)
+        foodlist = numpy.ones([1,len(impLocations)])[0]
         ml = makeMemoryList(noi)
         
         test0 = proxlist
         
         #Generate a new proximity list
-        proxlist = numpy.zeros([3, .5*len(plants)*(len(plants)-1)])
+        proxlist = numpy.zeros([3, .5*len(impLocations)*(len(impLocations)-1)])
         ccd1 = 0
         tct = 0
-        while ccd1 < len(plants):
-            for ccd2 in range(ccd1+1, len(plants)):
+        while ccd1 < len(impLocations):
+            for ccd2 in range(ccd1+1, len(impLocations)):
                 proxlist[0][tct] = ccd1
                 proxlist[1][tct] = ccd2
-                dis = numpy.linalg.norm(plants[ccd1]-plants[ccd2])
+                dis = numpy.linalg.norm(impLocations[ccd1]-impLocations[ccd2])
                 proxlist[2][tct] = 1/(dis*dis)
                 tct += 1
             ccd1 += 1
         
-        proxlist[2]= numpy.clip(proxlist[2::][0], 0, egalit*numpy.average(proxlist[2::][0]))
+        proxlist[2]= numpy.clip(proxlist[2::][0], 0, egalitarianism*numpy.average(proxlist[2::][0]))
